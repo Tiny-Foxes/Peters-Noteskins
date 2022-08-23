dc3c115f62644266796c7c7225a4f3dbe2e68a66
Lirodon <shawn.thompson306@gmail.com>
2022-08-23 00:59:25 -0600

    idea to try and make proW1 bit more distinct

--- OutFox/cel-3D/Fallback Explosion.lua
+++ OutFox/cel-3D/Fallback Explosion.lua
@@ -10,11 +10,11 @@ local cw4 = BoostColor(JudgmentLineToColor("JudgmentLine_W4"), 1.1)
 local cw3 = BoostColor(JudgmentLineToColor("JudgmentLine_W3"), 1.1)
 local cw2 = BoostColor(JudgmentLineToColor("JudgmentLine_W2"), 1.1)
 local cw1 = BoostColor(JudgmentLineToColor("JudgmentLine_W1"), 1.1)
-local cwpro5 = BoostColor(JudgmentLineToColor("JudgmentLine_ProW5"), 1.1)
-local cwpro4 = BoostColor(JudgmentLineToColor("JudgmentLine_ProW4"), 1.1)
-local cwpro3 = BoostColor(JudgmentLineToColor("JudgmentLine_ProW3"), 1.1)
-local cwpro2 = BoostColor(JudgmentLineToColor("JudgmentLine_ProW2"), 1.1)
-local cwpro1 = BoostColor(JudgmentLineToColor("JudgmentLine_ProW1"), 1.1)
+local cwpro5 = BoostColor(JudgmentLineToColor("JudgmentLine_ProW5"), 1.2)
+local cwpro4 = BoostColor(JudgmentLineToColor("JudgmentLine_ProW4"), 1.2)
+local cwpro3 = BoostColor(JudgmentLineToColor("JudgmentLine_ProW3"), 1.2)
+local cwpro2 = BoostColor(JudgmentLineToColor("JudgmentLine_ProW2"), 1.2)
+local cwpro1 = BoostColor(JudgmentLineToColor("JudgmentLine_ProW1"), 1.2)
 
 return Def.ActorFrame {
 	--Hold Explosion Commands
@@ -37,11 +37,11 @@ return Def.ActorFrame {
 		W3Command=function (self) self:finishtweening():stopeffect():diffuse(cw3):diffusealpha(1.2):zoom(1.1):accelerate(0.15):zoom(1):diffusealpha(0) end,
 		W2Command=function (self) self:finishtweening():stopeffect():diffuse(cw2):diffusealpha(1.2):zoom(1.1):accelerate(0.15):zoom(1):diffusealpha(0) end,
 		W1Command=function (self) self:finishtweening():diffuse(cw1):diffusealpha(1.2):zoom(1.1):accelerate(0.15):zoom(1):diffusealpha(0):glowshift():effectperiod(0.05):effectcolor1(1,1,1,0):effectcolor2(1,1,1,0.5) end,
-		ProW1Command=function (self) self:finishtweening():diffuse(cwpro1):diffusealpha(1.2):zoom(1.1):accelerate(0.15):zoom(1):diffusealpha(0):glowshift():effectperiod(0.05):effectcolor1(1,1,1,0):effectcolor2(1,1,1,0.5) end,
-		ProW2Command=function (self) self:finishtweening():diffuse(cwpro2):diffusealpha(1.2):zoom(1.1):accelerate(0.15):zoom(1):diffusealpha(0):glowshift():effectperiod(0.05):effectcolor1(1,1,1,0):effectcolor2(1,1,1,0.5) end,
-		ProW3Command=function (self) self:finishtweening():diffuse(cwpro3):diffusealpha(1.2):zoom(1.1):accelerate(0.15):zoom(1):diffusealpha(0):glowshift():effectperiod(0.05):effectcolor1(1,1,1,0):effectcolor2(1,1,1,0.5) end,
-		ProW4Command=function (self) self:finishtweening():diffuse(cwpro4):diffusealpha(1.2):zoom(1.1):accelerate(0.15):zoom(1):diffusealpha(0):glowshift():effectperiod(0.05):effectcolor1(1,1,1,0):effectcolor2(1,1,1,0.5) end,
-		ProW5Command=function (self) self:finishtweening():diffuse(cwpro5):diffusealpha(1.2):zoom(1.1):accelerate(0.15):zoom(1):diffusealpha(0):glowshift():effectperiod(0.05):effectcolor1(1,1,1,0):effectcolor2(1,1,1,0.5) end,
+		ProW1Command=function (self) self:finishtweening():diffuse(cwpro1):diffusealpha(1.2):zoom(1.2):accelerate(0.15):zoom(1):diffusealpha(0):glowshift():effectperiod(0.05):effectcolor1(1,1,1,0):effectcolor2(1,1,1,0.5) end,
+		ProW2Command=function (self) self:finishtweening():diffuse(cwpro2):diffusealpha(1.2):zoom(1.2):accelerate(0.15):zoom(1):diffusealpha(0):glowshift():effectperiod(0.05):effectcolor1(1,1,1,0):effectcolor2(1,1,1,0.5) end,
+		ProW3Command=function (self) self:finishtweening():diffuse(cwpro3):diffusealpha(1.2):zoom(1.2):accelerate(0.15):zoom(1):diffusealpha(0):glowshift():effectperiod(0.05):effectcolor1(1,1,1,0):effectcolor2(1,1,1,0.5) end,
+		ProW4Command=function (self) self:finishtweening():diffuse(cwpro4):diffusealpha(1.2):zoom(1.2):accelerate(0.15):zoom(1):diffusealpha(0):glowshift():effectperiod(0.05):effectcolor1(1,1,1,0):effectcolor2(1,1,1,0.5) end,
+		ProW5Command=function (self) self:finishtweening():diffuse(cwpro5):diffusealpha(1.2):zoom(1.2):accelerate(0.15):zoom(1):diffusealpha(0):glowshift():effectperiod(0.05):effectcolor1(1,1,1,0):effectcolor2(1,1,1,0.5) end,
 		JudgmentCommand=function(self) self:finishtweening() end,
 		BrightCommand=function(self) self:visible(true) end,
 		DimCommand=function(self) self:visible(true) end
